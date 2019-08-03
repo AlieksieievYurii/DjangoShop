e162f5369ed47cda0b7ccc5a7223fb1f1e7432fa
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-08-03 13:23:25 +0200

    Restruct urls

--- WebShop/WebShop/urls.py
+++ WebShop/WebShop/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
-from accomap.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', hello)
+    path('accomap/', include('accomap.urls')),
 ]
 
 if settings.DEBUG:
