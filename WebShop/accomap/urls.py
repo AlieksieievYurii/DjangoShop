9b4b52e5a90c658daec14bd9fd40bcb6c4ba9943
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-08-03 15:41:48 +0200

    Create new views

--- WebShop/accomap/urls.py
+++ WebShop/accomap/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import base_view
+from .views import product_view
+from .views import category_view
 
 urlpatterns = [
-    path('index/', base_view)
+    path('index/', base_view),
+    path('category/<str:category_slug>/', category_view),
+    path('product/<str:product_slug>/', product_view)
 ]
