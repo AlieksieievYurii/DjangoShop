7f9e884708fbbbc3c7556bfa96ac56e98a4cb53f
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-08-03 17:49:10 +0200

    Implement categories

--- WebShop/accomap/views.py
+++ WebShop/accomap/views.py
@@ -16,17 +16,21 @@ def base_view(request):
 
 
 def product_view(request, product_slug):
+    categories = Category.objects.all()
     product = Product.objects.get(slug=product_slug)
     context: Dict[str, any] = {
-        'product': product
+        'product': product,
+        'categories': categories
     }
     return render(request, 'product.html', context=context)
 
 
 def category_view(request, category_slug):
+    categories = Category.objects.all()
     category = Category.objects.get(slug=category_slug)
     products_of_category = category.product_set.all()
     context: Dict[str, any] = {
+        'categories': categories,
         'category': category,
         'products_of_category': products_of_category
     }
