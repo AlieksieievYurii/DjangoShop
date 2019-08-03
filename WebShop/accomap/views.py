64ea78cc014e9496313d1dc370ca2362988748e4
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-08-03 14:07:17 +0200

    Change views

--- WebShop/accomap/views.py
+++ WebShop/accomap/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from typing import Dict
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Category
+from .models import Product
+
+
+def base_view(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    context: Dict[str, any] = {
+        'categories': categories,
+        'products': products
+    }
+    return render(request, 'base.html', context=context)
