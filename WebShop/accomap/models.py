2bdbec482a855b9d89ed111a0b4084f7b1f75354
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-08-03 13:01:29 +0200

    Fix

--- WebShop/accomap/models.py
+++ WebShop/accomap/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+# Utils for models
+def _image_folder(instance, file_name):
+    file_name = '{slug}.{format}'.format(slug=instance.slug, format=file_name.split('.')[1])
+    return '{}/{}'.format(instance.slug, file_name)
+
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -23,7 +29,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField()
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to=_image_folder)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     available = models.BooleanField(default=True)
 
