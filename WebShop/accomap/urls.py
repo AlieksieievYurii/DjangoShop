cbfc8d176427a288d68ff58563756ce3d6688145
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-08-04 15:44:18 +0200

    Register url for the cart

--- WebShop/accomap/urls.py
+++ WebShop/accomap/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import base_view
 from .views import product_view
 from .views import category_view
+from .views import cart_view
 
 urlpatterns = [
     path('', base_view),
     path('category/<category_slug>/', category_view, name='category_view'),
-    path('product/<product_slug>/', product_view, name='product_view')
+    path('product/<product_slug>/', product_view, name='product_view'),
+    path('cart/', cart_view, name='cart'),
 ]
