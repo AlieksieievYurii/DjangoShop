9d5eca753265c922143ef34c15930adb6fcf23e2
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-08-04 17:02:28 +0200

    Add url for cart

--- WebShop/accomap/urls.py
+++ WebShop/accomap/urls.py
@@ -3,10 +3,12 @@ from .views import base_view
 from .views import product_view
 from .views import category_view
 from .views import cart_view
+from .views import add_to_cart_view
 
 urlpatterns = [
     path('', base_view),
     path('category/<category_slug>/', category_view, name='category_view'),
     path('product/<product_slug>/', product_view, name='product_view'),
     path('cart/', cart_view, name='cart'),
+    path('add_to_cart/<product_slug>', add_to_cart_view, name='add_to_cart'),
 ]
