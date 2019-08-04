aebc5d5413c3408b1909afc792bd495ede6045b5
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-08-04 19:09:49 +0200

    Remove params

--- WebShop/accomap/urls.py
+++ WebShop/accomap/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('category/<category_slug>/', category_view, name='category_view'),
     path('product/<product_slug>/', product_view, name='product_view'),
     path('cart/', cart_view, name='cart'),
-    path('add_to_cart/<product_slug>', add_to_cart_view, name='add_to_cart'),
-    path('remove_item_from_cart/<product_slug>', remove_from_card_view, name='remove_product')
+    path('add_to_cart/', add_to_cart_view, name='add_to_cart'),
+    path('remove_item_from_cart/', remove_from_card_view, name='remove_product')
 ]
