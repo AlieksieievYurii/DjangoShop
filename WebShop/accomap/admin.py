3537aa101a36f2c926c50f305390b911af4bfeb3
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-08-04 15:42:48 +0200

    Register new models for admin

--- WebShop/accomap/admin.py
+++ WebShop/accomap/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import Category
 from .models import Brand
 from .models import Product
+from .models import CartItem
+from .models import Cart
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Brand)
 admin.site.register(Product)
+admin.site.register(CartItem)
+admin.site.register(Cart)
